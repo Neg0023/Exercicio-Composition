50a0d4d556c935158aa24c7e00784b66585ff2e9
Uemerson Santos <uemerson161@hotmail.com>
2022-05-28 14:12:02 -0300

    Primeira parte da classe Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,32 @@
 package application;
 
+import entities.Client;
+import entities.enums.OrderStatus;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) throws ParseException {
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("Enter client data:");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+        System.out.print("Birth Date: ");
+        Date birthDate = sdf.parse(sc.next());
+
+        Client client = new Client(name, email, birthDate);
+
+        System.out.println("Enter order data:");
+        System.out.print("Status: ");
+        OrderStatus orderStatus = OrderStatus.valueOf(sc.next());
+        System.out.print("How many items to this order? ");
+        int n = sc.nextInt();
+    }
 }
